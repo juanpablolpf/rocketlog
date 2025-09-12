397e522a9c07095266e7bbb44e250d439f9632e1
Juan Pablo Filho <juan.pablo290102@gmail.com>
2025-09-12 15:16:08 -0300

    showing delivery logs

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -43,6 +43,10 @@ class DeliveryLogsController {
 
         const delivery = await prisma.delivery.findUnique({
             where: {id: delivery_id},
+            include: {
+                logs: true,
+                user: true
+            }
         })
 
         if (request.user?.role === "customer" && request.user.id !== delivery?.userId) {
